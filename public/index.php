ac9b7447c1acf75cbe46155b8b451538ea59dd4a
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-10-29 21:17:26 -0300

    Simples conexao com banco de dados e routerização

--- public/index.php
+++ public/index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "Olá mundo";
+// pegando a url que o usuario ta acessando
+$url = $_SERVER['REQUEST_URI'];  
 
-include "../src/teste.php";
+$pagina = match($url) {
+    '/' => 'home',
+    '/admin' => 'admin', //nome da view
+    '/admin/usuarios' => 'usuarios',
+    default => '404',
+};
+
+view($pagina);
+
+function view(string $nome): void
+{
+    include "../src/views/_layout/head.phtml";
+
+    include "../src/views/_components/menu.phtml";
+    include "../src/views/{$nome}.phtml";
+
+    include "../src/views/_layout/footer.phtml";
+}
